f7655a33f4272336163924aee62470b4ba3a9326
tknorris <tknorris@tknorris-laptop>
2016-01-30 12:53:34 -0500

    remove dead resolvers; add more fixed resolvers from Lambda

--- lib/urlresolver/plugins/flashx.py
+++ lib/urlresolver/plugins/flashx.py
@@ -38,33 +38,20 @@ class FlashxResolver(Plugin, UrlResolver, PluginSettings):
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'Referer': web_url}
-        stream_url = self.__get_link(web_url, media_id, headers)
-        if stream_url is None:
-            headers['User-Agent'] = common.IOS_USER_AGENT
-            stream_url = self.__get_link(web_url, media_id, headers)
-        
-        if stream_url is not None:
-            return stream_url + '|User-Agent=%s' % (common.IE_USER_AGENT)
+        html = self.net.http_GET(web_url).content
 
-        raise UrlResolver.ResolverError('File not found')
+        r = re.search('href="([^"]+)', html)
+        if r:
+            web_url = r.group(1)
+            html = self.net.http_GET(web_url).content
 
-    def __get_link(self, web_url, media_id, headers):
-        html = self.net.http_GET(web_url, headers=headers).content
-        data = {}
-        r = re.findall(r'type="hidden"\s*name="([^"]+)"\s*value="([^"]+)', html)
-        for name, value in r:
-            data[name] = value
-        data['imhuman'] = 'Proceed+to+video'
-        url = 'http://www.flashx.tv/dl?%s' % (media_id)
-        xbmc.sleep(6000)
-        html = self.net.http_POST(url, form_data=data, headers=headers).content
-        
-        best = 0
-        best_link = ''
-        for packed in re.findall("<script[^>]*>(eval\(function.*?)</script>", html, re.DOTALL):
-            unpacked = jsunpack.unpack(packed)
-            for stream in re.findall('file\s*:\s*"(http.*?)"\s*,\s*label\s*:\s*"(\d+)', unpacked, re.DOTALL):
+            try: html = jsunpack.unpack(re.search('(eval\(function.*?)</script>', html, re.DOTALL).group(1))
+            except: pass
+
+            best = 0
+            best_link = ''
+
+            for stream in re.findall('file\s*:\s*"(http.*?)"\s*,\s*label\s*:\s*"(\d+)', html, re.DOTALL):
                 if int(stream[1]) > best:
                     best = int(stream[1])
                     best_link = stream[0]
@@ -75,7 +62,7 @@ class FlashxResolver(Plugin, UrlResolver, PluginSettings):
             raise UrlResolver.ResolverError('Unable to resolve Flashx link. Filelink not found.')
         
     def get_url(self, host, media_id):
-        return 'http://www.flashx.tv/%s.html' % media_id
+        return 'http://www.flashx.tv/embed-%s.html' % media_id
 
     def get_host_and_id(self, url):
         r = re.search(self.pattern, url)
