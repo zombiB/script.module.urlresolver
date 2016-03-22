cba71e062002dd44e70fdfd74f19818d2032ad01
Tim Andrews <anxdpanic@gmail.com>
2016-03-22 18:00:54 -0400

    undo linebreak/whitespace

--- lib/urlresolver/plugins/openload.py
+++ lib/urlresolver/plugins/openload.py
@@ -36,8 +36,7 @@ class OpenLoadResolver(UrlResolver):
 
     def get_media_url(self, host, media_id):
         def baseN(num, b, numerals="0123456789abcdefghijklmnopqrstuvwxyz"):
-            return ((num == 0) and numerals[0]) or (
-                baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
+            return ((num == 0) and numerals[0]) or (baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
 
         def conv(s, addfactor=None):
             if 'function()' in s:
