85fdd2ac5e558b6a69a66216223c2867744981c5
Gujal00 <gujalajal@yahoo.co.in>
2016-04-15 21:37:32 +1200

    Revert "New resolver"
    This reverts commit c22c23abe10072d3a9069b3eea10dadcb2212202.

--- lib/urlresolver/plugins/nowvideo.py
+++ lib/urlresolver/plugins/nowvideo.py
@@ -22,8 +22,8 @@ from urlresolver.resolver import UrlResolver, ResolverError
 
 class NowvideoResolver(UrlResolver):
     name = "nowvideo"
-    domains = ['nowvideo.eu', 'nowvideo.ch', 'nowvideo.sx', 'nowvideo.co', 'nowvideo.li', 'nowvideo.fo']
-    pattern = '(?://|\.)(nowvideo\.(?:eu|ch|sx|co|li|fo))/(?:video/|embed\.php\?v=)([A-Za-z0-9]+)'
+    domains = ['nowvideo.eu', 'nowvideo.ch', 'nowvideo.sx', 'nowvideo.co', 'nowvideo.li', 'nowvideo.at']
+    pattern = '(?://|\.)(nowvideo\.(?:eu|ch|sx|co|li|at))/(?:video/|embed\.php\?v=)([A-Za-z0-9]+)'
 
     def __init__(self):
         self.net = common.Net()
