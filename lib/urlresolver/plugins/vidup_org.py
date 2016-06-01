58cd4cec3a9af94390c30373a592ee0a742c84f4
azzy9 <azzy9@hotmail.com>
2016-06-01 20:46:45 +0100

    Split Vidup.org and vidup.me plugin

--- lib/urlresolver/plugins/vidup_org.py
+++ lib/urlresolver/plugins/vidup_org.py
@@ -1,7 +1,7 @@
 """
     urlresolver XBMC Addon
     Copyright (C) 2011 t0mm0
-
+ 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
