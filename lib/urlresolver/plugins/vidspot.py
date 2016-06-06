3a3f828291728860be1e7ebbfab798060524f5bd
tknorris <tknorris@tknorris-laptop>
2016-06-06 16:08:09 -0400

    fix vidspot

--- lib/urlresolver/plugins/vidspot.py
+++ lib/urlresolver/plugins/vidspot.py
@@ -53,7 +53,7 @@ class VidSpotResolver(UrlResolver):
             raise ResolverError('could not find sources')
 
     def get_url(self, host, media_id):
-        return 'http://vidspot.net/%s' % media_id
+        return 'http://vidspot.net/embed-%s.html' % (media_id)
 
     def get_host_and_id(self, url):
         r = re.search(self.pattern, url)
