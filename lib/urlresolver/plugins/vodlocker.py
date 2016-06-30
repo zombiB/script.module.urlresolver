ede9da139efe62012a24fbd841b16cbc399ec195
tknorris <tknorris@tknorris-laptop>
2016-06-30 15:05:41 -0400

    remove get_host_and_id

--- lib/urlresolver/plugins/vodlocker.py
+++ lib/urlresolver/plugins/vodlocker.py
@@ -43,10 +43,3 @@ class VodlockerResolver(UrlResolver):
 
     def get_url(self, host, media_id):
         return 'http://vodlocker.com/embed-%s-640x400.html' % media_id
-
-    def get_host_and_id(self, url):
-        r = re.search(self.pattern, url)
-        if r:
-            return r.groups()
-        else:
-            return False
