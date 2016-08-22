f49f55986a037e2c9784082d630806ca6fd74e94
tknorris <tknorris@tknorris-laptop>
2016-08-22 13:54:04 -0400

    add ip auth logic to thevideo.me

--- lib/urlresolver/plugins/thevideo.py
+++ lib/urlresolver/plugins/thevideo.py
@@ -15,14 +15,15 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
-import os
-import hashlib
+import re
+import json
+import time
+from lib import helpers
 from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
 
 MAX_TRIES = 3
-PY_SOURCE = 'https://offshoregit.com/tvaresolvers/thevideo_gmu.py'
-PY_PATH = os.path.join(common.plugins_path, 'thevideo_gmu.py')
+INTERVALS = 5
 
 class TheVideoResolver(UrlResolver):
     name = "thevideo"
@@ -31,31 +32,48 @@ class TheVideoResolver(UrlResolver):
 
     def __init__(self):
         self.net = common.Net()
+        self.headers = {'User-Agent': common.SMU_USER_AGENT}
 
-    @common.cache.cache_method(cache_limit=3)
-    def get_gmu_code(self):
-        try:
-            new_py = self.net.http_GET(PY_SOURCE).content
-            if new_py:
-                with open(PY_PATH, 'w') as f:
-                    f.write(new_py)
-        except Exception as e:
-            common.log_utils.log_warning('Exception during thevideo code retrieve: %s' % e)
-            
     def get_media_url(self, host, media_id):
-        try:
-            if self.get_setting('auto_update') == 'true':
-                self.get_gmu_code()
-            with open(PY_PATH, 'r') as f:
-                py_data = f.read()
-            common.log_utils.log('thevideo_gmu hash: %s' % (hashlib.md5(py_data).hexdigest()))
-            import thevideo_gmu
-            web_url = self.get_url(host, media_id)
-            return thevideo_gmu.get_media_url(web_url, host, self.get_setting('auto_pick') == 'true')
-        except Exception as e:
-            common.log_utils.log_debug('Exception during thevideo resolve parse: %s' % e)
-            raise
-            
+        web_url = self.get_url(host, media_id)
+        headers = {
+            'Referer': web_url
+        }
+        headers.update(self.headers)
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = re.findall(r"'?label'?\s*:\s*'([^']+)p'\s*,\s*'?file'?\s*:\s*'([^']+)", html, re.I)
+        if sources:
+            vt = self.__auth_ip(media_id)
+            if vt:
+                source = helpers.pick_source(sources, self.get_setting('auto_pick') == 'true')
+                return '%s?direct=false&ua=1&vt=%s|User-Agent=%s' % (source, vt, common.SMU_USER_AGENT)
+        else:
+            raise ResolverError('Unable to locate links')
+
+    def __auth_ip(self, media_id):
+        header = 'TheVideo.me IP Authorization'
+        line1 = 'In order to stream this video you must authorize your IP'
+        line2 = 'Visit URL: https://thevideo.me/pair '
+        line3 = 'on [B]ANY DEVICE[/B] on the same network'
+        with common.kodi.ProgressDialog(header, line1=line1, line2=line2, line3=line3) as pd:
+            pd.update(100)
+            expires = time_left = 300  # give user 5 minutes
+            start = time.time()
+            interval = 5  # check url every 5 seconds
+            while time_left:
+                url = 'https://thevideo.me/pair?file_code=%s&check' % (media_id)
+                try: js_result = json.loads(self.net.http_GET(url, headers=self.headers).content)
+                except ValueError: raise ResolverError('Unusable Authorization Response')
+                if js_result.get('status'):
+                    return js_result.get('response', {}).get('vt')
+                
+                time_left = expires - int(time.time() - start)
+                progress = time_left * 100 / expires
+                pd.update(progress)
+                for _ in range(INTERVALS):
+                    common.kodi.sleep(interval * 1000 / INTERVALS)
+                    if pd.is_canceled(): return
+        
     def get_url(self, host, media_id):
         return 'http://%s/embed-%s.html' % (host, media_id)
 
@@ -63,5 +81,4 @@ class TheVideoResolver(UrlResolver):
     def get_settings_xml(cls):
         xml = super(cls, cls).get_settings_xml()
         xml.append('<setting id="%s_auto_pick" type="bool" label="Automatically pick best quality" default="false" visible="true"/>' % (cls.__name__))
-        xml.append('<setting id="%s_auto_update" type="bool" label="Automatically update resolver" default="true"/>' % (cls.__name__))
         return xml
