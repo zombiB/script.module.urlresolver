df42ce362cb8bd4f802baac137f0c2bc9defb2b1
tknorris <tknorris@new-laptop>
2016-11-10 22:00:10 -0500

    make flashx dynamic

--- lib/urlresolver/plugins/flashx.py
+++ lib/urlresolver/plugins/flashx.py
@@ -16,12 +16,13 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-import re
-from lib import jsunpack
-from lib import helpers
+import os
+import hashlib
 from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
+
+FX_SOURCE = 'https://offshoregit.com/tvaresolvers/fx_gmu.py'
+FX_PATH = os.path.join(common.plugins_path, 'fx_gmu.py')
 
 class FlashxResolver(UrlResolver):
     name = "flashx"
@@ -31,114 +32,49 @@ class FlashxResolver(UrlResolver):
     def __init__(self):
         self.net = common.Net()
 
+    @common.cache.cache_method(cache_limit=1)
+    def get_fx_code(self):
+        try:
+            headers = self.net.http_HEAD(FX_SOURCE).get_headers(as_dict=True)
+            common.log_utils.log(headers)
+            old_etag = self.get_setting('etag')
+            new_etag = headers.get('Etag', '')
+            old_len = self.__old_length()
+            new_len = int(headers.get('Content-Length', 0))
+            if old_etag != new_etag or old_len != new_len:
+                common.log_utils.log('Updating fx_gmu: |%s|%s|%s|%s|' % (old_etag, new_etag, old_len, new_len))
+                self.set_setting('etag', new_etag)
+                new_py = self.net.http_GET(FX_SOURCE).content
+                if new_py:
+                    with open(FX_PATH, 'w') as f:
+                        f.write(new_py)
+            else:
+                common.log_utils.log('Reusing existing fx_gmu.py: |%s|%s|%s|%s|' % (old_etag, new_etag, old_len, new_len))
+        except Exception as e:
+            common.log_utils.log_warning('Exception during flashx code retrieve: %s' % e)
+            
+    def __old_length(self):
+        try:
+            with open(FX_PATH, 'r') as f:
+                old_py = f.read()
+            old_len = len(old_py)
+        except:
+            old_len = 0
+        return old_len
+
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-
-        headers = {'User-Agent': common.FF_USER_AGENT}
-        response = self.net.http_GET(web_url, headers=headers)
-        html = response.content
-
-        cookie = response.get_headers(as_dict=True).get('Set-Cookie', {})
-        cookie.update(self.__get_cookies(html))
-        headers.update({'Cookie': cookie, 'Referer': 'http://%s' % host})
-
-        pattern = '<div[^>]*id\s*=\s*[\'"]main[\'"][^>]*>.*?[^"]+[\.|%s]\/(\w+\/\w+\.\w+).*?' % host  # api-js
-        pattern += '"([^"]+%s[^"]+(?:\d+|)\.\w{1,3}\?\w+=[^"]+)".*?' % host  # cgi
-        pattern += '<span[^>]*id=["|\']\s*\w+(?:\d+|)\s*["|\'][^>]*>(\d+)<'  # countdown
-        match = re.search(pattern, html, re.DOTALL | re.I)
-        # FlashX: If you are tired of this cat and mouse game then we have a pairing solution that would still let you get revenue and still allow kodi users to view your streams
-        # Read more about it here: https://www.tvaddons.ag/rickroll-thevideo/
-        # We've used this solution with OpenLoad, thevideo.me, vidup.me and others to good effect. Contact tknorris via PM @ https://forums.tvaddons.ag/ to talk details
-
-        if not match:
-            raise ResolverError('Site structure changed!')
-
-        jscontent = self.net.http_GET('http://%s/%s' % (host, match.group(1)), headers=headers).content
-        matchjs = re.search('!=\s*null.*?{(\w+):', jscontent, re.DOTALL | re.I)
-
-        if not matchjs:
-            raise ResolverError('Site structure changed!')
-
-        html = re.sub('<div[^>]*style\s*=\s*["|\']visibility\:(\s)?hidden["|\'][^>]*>.*?</div>', '', html, flags=re.MULTILINE | re.DOTALL | re.IGNORECASE)
-
-        postUrl = ''
-        for matchPff in re.finditer('var\s+s\s*=\s*["|\'](.*?)["|\']', html, re.DOTALL):
-            decStr = self.pff(matchPff.group(1))
-            matchAct = re.search('action\s*=\s*[\'"]([^\'"]+)[\'"].*?', decStr)
-            if matchAct:
-                html = html.replace(matchPff.group(1), decStr)
-                postUrl = matchAct.group(1)
-            else:
-                html += decStr
-
-        if not postUrl:
-            matchAct = re.search('<form[^>]*action\s*=\s*[\'"]([^\'"]+)[\'"].*?', html, re.DOTALL | re.I)
-            if matchAct:
-                postUrl = matchAct.group(1)
-
-        if postUrl and not host in postUrl:
-             postUrl = 'http://%s/%s' % (host, postUrl)
-
-        if not postUrl:
-            raise ResolverError('Site structure changed!')
-
-        self.net.http_GET('http://www.%s/flashx.php?%s=1' % (host, matchjs.group(1)), headers=headers)
-        self.net.http_GET(match.group(2).strip(), headers=headers)
-        data = self.get_postvalues(html)
-        common.kodi.sleep(int(match.group(3)) * 1000 + 500)
-        html = self.net.http_POST(postUrl, data, headers=headers).content
-
-        sources = []
-        for match in re.finditer('(eval\(function.*?)</script>', html, re.DOTALL):
-            packed_data = jsunpack.unpack(match.group(1))
-            sources += self.__parse_sources_list(packed_data)
-
-        if len(sources) > 0:
-            return helpers.pick_source(sources)
-
-        raise ResolverError('Unable to find video')
-
-    @staticmethod
-    def pff(decStr):
-        m=""
-        for i in range(0, len(decStr)):
-            if ord(decStr[i]) == 28:
-                m += '&'
-            elif ord(decStr[i]) == 23:
-                m += '!'
-            else:
-                m += chr(ord(decStr[i]) - 1)
-        return m
-
-    def get_postvalues(self, html):
-        postvals = {}
-        valid_names = ['hash', 'imhuman', 'usr_login', 'referer', 'fname', 'id', 'op']
-
-        for i, form in enumerate(re.finditer('''<form[^>|]+>(.*?)</form>''', html, re.DOTALL | re.I)):
-            for field in re.finditer('''<input [^>]*type=['"]?[hidden|submit]['"]?[^>]*>''', form.group(1)):
-                match = re.search('''name\s*=\s*['"]([^'"]+)''', field.group(0))
-                match1 = re.search('''value\s*=\s*['"]([^'"]*)''', field.group(0))
-                if match and match1 and match.group(1).lower() in valid_names:
-                    postvals[match.group(1)] = match1.group(1)
-
-        return postvals
-
-    def __get_cookies(self, html):
-        cookies = {}
-        for match in re.finditer("\$\.cookie\(\s*'([^']+)'\s*,\s*'([^']+)", html):
-            key, value = match.groups()
-            cookies[key] = value
-        return cookies
-
-    def __parse_sources_list(self, html):
-        sources = []
-        match = re.search('sources\s*:\s*\[(.*?)\]', html, re.DOTALL)
-        if match:
-            for match in re.finditer('''['"]?file['"]?\s*:\s*['"]([^'"]+)['"][^}]*['"]?label['"]?\s*:\s*['"]([^'"]*)''', match.group(1), re.DOTALL):
-                stream_url, label = match.groups()
-                stream_url = stream_url.replace('\/', '/')
-                sources.append((label, stream_url))
-        return sources
-
+        try:
+            if self.get_setting('auto_update') == 'true':
+                self.get_fx_code()
+            with open(FX_PATH, 'r') as f:
+                py_data = f.read()
+            common.log_utils.log('fx_gmu hash: %s' % (hashlib.md5(py_data).hexdigest()))
+            import fx_gmu
+            web_url = self.get_url(host, media_id)
+            return fx_gmu.get_media_url(web_url)
+        except Exception as e:
+            common.log_utils.log_debug('Exception during flashx resolve parse: %s' % e)
+            raise
+        
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'http://{host}/{media_id}')
+        return self._default_get_url(host, media_id, 'http://{host}/embed.php?c={media_id}')
---
+++ lib/urlresolver/plugins/fx_gmu.py
@@ -0,0 +1,48 @@
+"""
+flashx.tv urlresolver plugin
+Copyright (C) 2015 tknorris
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+import re
+from lib import jsunpack
+from lib import helpers
+from urlresolver import common
+from urlresolver.resolver import ResolverError
+
+SORT_KEY = {'High': 3, 'Middle': 2, 'Low': 1}
+
+def get_media_url(url):
+    try:
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        net = common.Net()
+        html = net.http_GET(url, headers=headers).content
+        match = re.search('''href=['"]([^'"]+)''', html)
+        if match:
+            html = net.http_GET(match.group(1), headers=headers).content
+            headers.update({'Referer': url})
+            for match in re.finditer('(eval\(function.*?)</script>', html, re.DOTALL):
+                html += jsunpack.unpack(match.group(1))
+        
+        sources = helpers.parse_sources_list(html)
+        try: sources.sort(key=lambda x: SORT_KEY.get(x[0], 0), reverse=True)
+        except: pass
+        source = helpers.pick_source(sources)
+        return source + helpers.append_headers(headers)
+        
+    except Exception as e:
+        common.log_utils.log_debug('Exception during flashx resolve parse: %s' % e)
+        raise
+    
+    raise ResolverError('Unable to resolve flashx link. Filelink not found.')
