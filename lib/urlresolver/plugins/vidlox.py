6710a8ad7caa9bf3927cde72652e9e17ce11761b
alifrezser <darkraver001@gmail.com>
2016-11-19 19:09:11 +0100

    Revert "fix vidlox"
    This reverts commit 5be80a3cb18ce3a5e818c90e1093d26611836f88.

--- lib/urlresolver/plugins/vidlox.py
+++ lib/urlresolver/plugins/vidlox.py
@@ -19,37 +19,17 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
 from lib import helpers
-from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
+
 
 class VidloxResolver(UrlResolver):
     name = "vidlox"
     domains = ['vidlox.tv']
-    pattern = '(?://|\.)(vidlox\.tv)/(?:embed-|)([0-9a-zA-Z]+)'
-
-    def __init__(self):
-        self.net = common.Net()
+    pattern = '(?://|\.)(vidlox\.tv)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT, 'Referer': web_url}
-        html = self.net.http_GET(web_url, headers=headers).content
-
-        sources = helpers.parse_sources_list(html)
-        
-        if sources:
-            source = helpers.pick_source(sources, self.get_setting('auto_pick') == 'true')
-            return source + helpers.append_headers(headers)
-        
-        raise ResolverError('No playable video found.')
+        return helpers.get_media_url(self.get_url(host, media_id), result_blacklist=['dl'])
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id)
-
-    @classmethod
-    def get_settings_xml(cls):
-        xml = super(cls, cls).get_settings_xml()
-        xml.append('<setting id="%s_auto_pick" type="bool" label="Automatically pick best quality" default="false" visible="true"/>' % (cls.__name__))
-        return xml
+        return self._default_get_url(host, media_id, 'http://{host}/{media_id}')
