c735a7714464cfe10e061a54e7a93434a1763ca6
tknorris <tknorris@new-laptop>
2016-11-29 19:49:43 -0500

    update to https

--- lib/urlresolver/plugins/flashx.py
+++ lib/urlresolver/plugins/flashx.py
@@ -78,7 +78,7 @@ class FlashxResolver(UrlResolver):
             raise
         
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'http://{host}/embed.php?c={media_id}')
+        return self._default_get_url(host, media_id, 'https://{host}/embed.php?c={media_id}')
 
     @classmethod
     def get_settings_xml(cls):
