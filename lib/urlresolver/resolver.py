df300bcf61031e9a8d63644ca426ab9946ee78a0
tknorris <tknorris@new-laptop>
2016-12-27 14:36:36 -0500

    delete old compiled file on py update

--- lib/urlresolver/resolver.py
+++ lib/urlresolver/resolver.py
@@ -195,6 +195,8 @@ class UrlResolver(object):
                             new_py = common.decrypt_py(new_py, key)
                             
                         if new_py and 'import' in new_py:
+                            try: os.remove(re.sub('\.py$', '.pyo', py_path))
+                            except: pass
                             with open(py_path, 'w') as f:
                                 f.write(new_py)
                             common.kodi.notify('%s %s' % (self.name, common.i18n('resolver_updated')))
