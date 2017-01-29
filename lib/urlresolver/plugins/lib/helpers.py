7fbdc7dccf927827cc1541880270ee02803b1ad8
tknorris <tknorris@new-laptop>
2017-01-28 21:08:10 -0500

    convert vidto to generic resolver; add additional parse scrape

--- lib/urlresolver/plugins/lib/helpers.py
+++ lib/urlresolver/plugins/lib/helpers.py
@@ -138,12 +138,14 @@ def scrape_sources(html, result_blacklist=None, scheme='http'):
     html = add_packed_data(html)
 
     source_list = []
+    source_list += __parse_to_list(html, '''["']?label\s*["']?\s*[:=]\s*["'](?P<label>[^"']+)["'](?:,|[^}\]])["']?\s*file\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)''')
     source_list += __parse_to_list(html, '''["']?\s*file\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\],]?["',]?\s*label\s*["']?\s*[:=]?\s*["'](?P<label>[^"']+))?''')
     source_list += __parse_to_list(html, '''video[^><]+src\s*=\s*['"](?P<url>[^'"]+)''')
     source_list += __parse_to_list(html, '''source\s+src\s*=\s*['"](?P<url>[^'"]+)['"](?:.*?data-res\s*=\s*['"](?P<label>[^'"]+))?''')
     source_list += __parse_to_list(html, '''["']?\s*url\s*["']?\s*[:=]\s*["'](?P<url>[^"']+)''')
     source_list += __parse_to_list(html, '''param\s+name\s*=\s*"src"\s*value\s*=\s*"(?P<url>[^"]+)''')
 
+    common.log_utils.log(source_list)
     if len(source_list) > 1:
         try: source_list.sort(key=lambda x: int(x[0]), reverse=True)
         except:
